adf7eb3efc54965c0bca4a4d814a577546343ba0
ZhiyanOlexandr <zhiyan.olexandr@gmail.com>
2022-07-12 12:27:11 +0300

    update

---
+++ OZ/Sniffs/Formatting/EmptyLineBeforeReturnSniff.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ZhiyanOlexandr\Sniffs\Formatting;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class EmptyLineBeforeReturnSniff implements Sniff
+{
+    public function register()
+    {
+
+    }
+
+    public function process( File $phpcsFile, $stackPtr )
+    {
+
+    }
+}
